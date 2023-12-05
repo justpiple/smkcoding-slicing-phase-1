09afe0e43b8aba3c868bf5f38a08ffed8d56f88b
Kusindra Aji Rabbany <me@benspace.xyz>
2023-12-05 21:57:13 +0700

    upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "blue": "#421ae6",
+        "blue-secondary": "#f0f3ff",
+        "yellow": "#facb3b"
+      }
+    },
+  },
+  plugins: [],
+}
+
